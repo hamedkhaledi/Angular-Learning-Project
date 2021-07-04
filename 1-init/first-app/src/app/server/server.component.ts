2aa57a59554538e05c4238052d99cd4f89546af9
Hamed Khademi Khaledi <khaledihkh@gmail.com>
2021-07-04 16:35:15 +0430

    color style with ngClass

--- 1-init/first-app/src/app/server/server.component.ts
+++ 1-init/first-app/src/app/server/server.component.ts
@@ -3,7 +3,13 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-server',
   templateUrl: './server.component.html',
-  styleUrls: ['./server.component.css']
+  styles: [
+    `
+      .online {
+        color: white
+      }
+    `
+  ]
 })
 export class ServerComponent implements OnInit {
   serverId = 10;
