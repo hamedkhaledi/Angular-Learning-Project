a8273a5341686a2bb8fab16dd9666ca4efe072e0
Hamed Khademi Khaledi <khaledihkh@gmail.com>
2021-07-04 16:45:49 +0430

    list of srevers

--- 1-init/first-app/src/app/servers/servers.component.ts
+++ 1-init/first-app/src/app/servers/servers.component.ts
@@ -10,7 +10,7 @@ export class ServersComponent implements OnInit {
   serverCreationStatus = 'No server was created';
   serverName = '';
   serverCreated = false;
-
+  servers = ['testServer1', 'testServer2'];
   constructor() {
     setTimeout(() => {
       this.allowNewServer = true;
@@ -21,6 +21,7 @@ export class ServersComponent implements OnInit {
 
   onCreateServer() {
     this.serverCreated = true;
+    this.servers.push(this.serverName);
     this.serverCreationStatus =
       'Server was created! Name is ' + this.serverName;
   }
