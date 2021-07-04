adfd06aa169558d9d7dabb02b43efd94fef7cd0b
Hamed Khademi Khaledi <khaledihkh@gmail.com>
2021-07-04 15:27:27 +0430

    add button binding

--- 1-init/first-app/src/app/servers/servers.component.ts
+++ 1-init/first-app/src/app/servers/servers.component.ts
@@ -6,10 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  allowNewServer = false;
+  serverCreationStatus = 'No server was created';
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
   }
 
+  ngOnInit(): void {}
+
+  onCreateServer() {
+    this.serverCreationStatus = 'Server was created';
+  }
 }
