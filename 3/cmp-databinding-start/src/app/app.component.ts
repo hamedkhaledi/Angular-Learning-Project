79d18437264a5cba04e9f1d8e5c2a708f5385838
Hamed Khademi Khaledi <khaledihkh@gmail.com>
2021-07-09 19:28:36 +0430

    bind element

--- 3/cmp-databinding-start/src/app/app.component.ts
+++ 3/cmp-databinding-start/src/app/app.component.ts
@@ -6,23 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  serverElements = [];
-  newServerName = '';
-  newServerContent = '';
+  serverElements = [{type: 'server', name: 'Testserver', content: 'Just a test!'}];
+  
 
-  onAddServer() {
-    this.serverElements.push({
-      type: 'server',
-      name: this.newServerName,
-      content: this.newServerContent
-    });
-  }
-
-  onAddBlueprint() {
-    this.serverElements.push({
-      type: 'blueprint',
-      name: this.newServerName,
-      content: this.newServerContent
-    });
-  }
+  
 }
---
+++ 3/cmp-databinding-start/src/app/server-element/server-element.component.ts
@@ -0,0 +1,18 @@
+import { element } from 'protractor';
+import { Component, OnInit } from '@angular/core';
+import { Input } from '@angular/core';
+
+@Component({
+  selector: 'app-server-element',
+  templateUrl: './server-element.component.html',
+  styleUrls: ['./server-element.component.css']
+})
+export class ServerElementComponent implements OnInit {
+  @Input() 
+  element : {type:string,name:string,content:string}
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
