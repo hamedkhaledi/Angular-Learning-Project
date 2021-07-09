cf324f6272a9ce7c57c42c03182bdfad25ee3bf0
Hamed Khademi Khaledi <khaledihkh@gmail.com>
2021-07-09 19:51:52 +0430

    assign alias to event

--- 3/cmp-databinding-start/src/app/cockpit/cockpit.component.ts
+++ 3/cmp-databinding-start/src/app/cockpit/cockpit.component.ts
@@ -10,7 +10,7 @@ export class CockpitComponent implements OnInit {
   newServerContent = '';
 
   @Output() serverCreated = new EventEmitter<{ serverName: string; serverContent: string }>();
-  @Output() blueprintCreated = new EventEmitter<{ serverName: string; serverContent: string }>();
+  @Output('bpCreated') blueprintCreated = new EventEmitter<{ serverName: string; serverContent: string }>();
   constructor() { }
 
   ngOnInit(): void { }
