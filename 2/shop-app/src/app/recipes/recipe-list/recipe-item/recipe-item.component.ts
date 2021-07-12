3e47e7407fc96277b20abbf4ef9e67de05544b2f
Hamed Khademi Khaledi <khaledihkh@gmail.com>
2021-07-13 00:18:55 +0430

    seperate recipe list component

--- 2/shop-app/src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ 2/shop-app/src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,15 +1,15 @@
+import { Recipe } from './../../recipe.model';
 import { Component, OnInit } from '@angular/core';
+import { Input } from '@angular/core';
 
 @Component({
   selector: 'app-recipe-item',
   templateUrl: './recipe-item.component.html',
-  styleUrls: ['./recipe-item.component.css']
+  styleUrls: ['./recipe-item.component.css'],
 })
 export class RecipeItemComponent implements OnInit {
+  @Input() recipe: Recipe;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
