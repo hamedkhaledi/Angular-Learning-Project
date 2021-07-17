e3f8b7277e470e6a01c0957e3cae678101535757
Hamed Khademi Khaledi <khaledihkh@gmail.com>
2021-07-17 06:56:53 +0430

    add highlight directive

--- 4/directives-start/src/app/app.module.ts
+++ 4/directives-start/src/app/app.module.ts
@@ -1,19 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
 
-
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+import { BasicHighlightDirective } from "./basic-highlight/basic-highlight.directive";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-  ],
+  declarations: [AppComponent, BasicHighlightDirective],
+  imports: [BrowserModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
