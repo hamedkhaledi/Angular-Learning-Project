50b01e39b43b7f632c102007613f6b3fe0dec70e
Hamed Khademi Khaledi <khaledihkh@gmail.com>
2021-07-17 07:17:32 +0430

    add better directive

--- 4/directives-start/src/app/app.module.ts
+++ 4/directives-start/src/app/app.module.ts
@@ -4,9 +4,10 @@ import { FormsModule } from "@angular/forms";
 
 import { AppComponent } from "./app.component";
 import { BasicHighlightDirective } from "./basic-highlight/basic-highlight.directive";
+import { BetterHighlightDirective } from './better-highlight/better-highlight.directive';
 
 @NgModule({
-  declarations: [AppComponent, BasicHighlightDirective],
+  declarations: [AppComponent, BasicHighlightDirective, BetterHighlightDirective],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ 4/directives-start/src/app/better-highlight/better-highlight.directive.ts
@@ -0,0 +1,11 @@
+import { Directive, ElementRef, OnInit, Renderer2 } from "@angular/core";
+
+@Directive({
+  selector: "[appBetterHighlight]",
+})
+export class BetterHighlightDirective implements OnInit {
+  constructor(private elRef: ElementRef, private renderer: Renderer2) {}
+  ngOnInit(): void {
+    this.renderer.setStyle(this.elRef.nativeElement,'background-color','blue');
+  }
+}
