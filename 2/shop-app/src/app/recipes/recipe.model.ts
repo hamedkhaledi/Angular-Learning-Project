066513872d7344cfe25a2d8a06539ee7302d5fe8
Hamed Khademi Khaledi <khaledihkh@gmail.com>
2021-07-19 01:19:26 +0430

    Adding Ingredients to Recipes

--- 2/shop-app/src/app/recipes/recipe.model.ts
+++ 2/shop-app/src/app/recipes/recipe.model.ts
@@ -1,11 +1,20 @@
+import { Ingredient } from '../Shared/ingredient.model';
+
 export class Recipe {
   public name: string;
   public description: string;
   public imagePath: string;
+  public ingredients: Ingredient[];
 
-  constructor(name: string, description: string, imagePath: string) {
+  constructor(
+    name: string,
+    description: string,
+    imagePath: string,
+    ingredients: Ingredient[]
+  ) {
     this.name = name;
     this.description = description;
     this.imagePath = imagePath;
+    this.ingredients = ingredients;
   }
 }
--- 2/shop-app/src/app/recipes/recipe.service.ts
+++ 2/shop-app/src/app/recipes/recipe.service.ts
@@ -1,20 +1,22 @@
+import { Ingredient } from '../Shared/ingredient.model';
 import { Recipe } from './recipe.model';
 
 export class RecipeService {
-
-    recipes: Recipe[] = [
-        new Recipe(
-            'A Test Recipe',
-            'This is simply a test',
-            'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'
-        ),
-        new Recipe(
-            'Another Test Recipe',
-            'This is simply a test',
-            'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'
-        ),
-    ];
-    getRecipes() {
-        return this.recipes.slice(); // copy array
-    }
+  private recipes: Recipe[] = [
+    new Recipe(
+      'Tasty Schnitzel',
+      'A super-tasty Schnitzel - just awesome!',
+      'https://upload.wikimedia.org/wikipedia/commons/7/72/Schnitzel.JPG',
+      [new Ingredient('Meat', 1), new Ingredient('French Fries', 20)]
+    ),
+    new Recipe(
+      'Big Fat Burger',
+      'What else you need to say?',
+      'https://upload.wikimedia.org/wikipedia/commons/b/be/Burger_King_Angus_Bacon_%26_Cheese_Steak_Burger.jpg',
+      [new Ingredient('Buns', 2), new Ingredient('Meat', 1)]
+    ),
+  ];
+  getRecipes() {
+    return this.recipes.slice(); // copy array
+  }
 }
