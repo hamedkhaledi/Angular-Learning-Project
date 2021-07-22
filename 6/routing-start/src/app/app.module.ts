7375f70ed59795644c276de91a5f63e015aa5801
Hamed Khademi Khaledi <khaledihkh@gmail.com>
2021-07-22 12:18:01 +0430

    Outsourcing the Route Configuration

--- 6/routing-start/src/app/app.module.ts
+++ 6/routing-start/src/app/app.module.ts
@@ -10,47 +10,8 @@ import { UserComponent } from "./users/user/user.component";
 import { EditServerComponent } from "./servers/edit-server/edit-server.component";
 import { ServerComponent } from "./servers/server/server.component";
 import { ServersService } from "./servers/servers.service";
-import { RouterModule, Routes } from "@angular/router";
 import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
-
-const appRoutes: Routes = [
-  {
-    path: "",
-    component: HomeComponent,
-  },
-  {
-    path: "users",
-    component: UsersComponent,
-    children: [
-      {
-        path: ":id/:name",
-        component: UserComponent,
-      },
-    ],
-  },
-  {
-    path: "servers",
-    component: ServersComponent,
-    children: [
-      {
-        path: ":id",
-        component: ServerComponent,
-      },
-      {
-        path: ":id/edit",
-        component: EditServerComponent,
-      },
-    ],
-  },
-  {
-    path: "not-found",
-    component: PageNotFoundComponent,
-  },
-  {
-    path: "**",
-    redirectTo: '/not-found',
-  },
-];
+import { AppRoutingModule } from "./app-routing.module";
 
 @NgModule({
   declarations: [
@@ -63,7 +24,7 @@ const appRoutes: Routes = [
     ServerComponent,
     PageNotFoundComponent,
   ],
-  imports: [BrowserModule, FormsModule, RouterModule.forRoot(appRoutes)],
+  imports: [BrowserModule, FormsModule, AppRoutingModule],
   providers: [ServersService],
   bootstrap: [AppComponent],
 })
