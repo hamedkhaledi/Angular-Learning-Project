7c92eea585948278aa98b83da884183fc03dd0fb
Hamed Khademi Khaledi <khaledihkh@gmail.com>
2021-07-27 23:57:47 +0430

    Operators

--- 7/obs-01-start/src/app/home/home.component.ts
+++ 7/obs-01-start/src/app/home/home.component.ts
@@ -2,7 +2,7 @@ import { OnDestroy } from "@angular/core";
 import { Component, OnInit } from "@angular/core";
 import { Observer } from "rxjs";
 import { interval, Observable, Subscription } from "rxjs";
-
+import { map, filter } from "rxjs/operators";
 @Component({
   selector: "app-home",
   templateUrl: "./home.component.html",
@@ -32,17 +32,27 @@ export class HomeComponent implements OnInit, OnDestroy {
         count++;
       }, 1000);
     });
-    this.firstObsSubscription = customIntervalObservable.subscribe(
-      (data) => {
-        console.log(data);
-      },
-      (error) => {
-        console.log(error);
-        alert(error.message);
-      },
-      () => {
-        console.log("Completed!");
-      }
-    );
+
+    this.firstObsSubscription = customIntervalObservable
+      .pipe(
+        filter((data) => {
+          return data > 0;
+        }),
+        map((data: number) => {
+          return "Round : " + (data + 1);
+        })
+      )
+      .subscribe(
+        (data) => {
+          console.log(data);
+        },
+        (error) => {
+          console.log(error);
+          alert(error.message);
+        },
+        () => {
+          console.log("Completed!");
+        }
+      );
   }
 }
