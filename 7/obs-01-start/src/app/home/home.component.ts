4bbf9fce08825361120ad1f8b5ab32e39c9deb6a
Hamed Khademi Khaledi <khaledihkh@gmail.com>
2021-07-27 23:33:06 +0430

    Building a Custom Observable

--- 7/obs-01-start/src/app/home/home.component.ts
+++ 7/obs-01-start/src/app/home/home.component.ts
@@ -1,32 +1,48 @@
-import { OnDestroy } from '@angular/core';
-import { Component, OnInit } from '@angular/core';
-import { Observer } from 'rxjs';
-import { interval, Observable, Subscription } from 'rxjs';
+import { OnDestroy } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { Observer } from "rxjs";
+import { interval, Observable, Subscription } from "rxjs";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.css"],
 })
-export class HomeComponent implements OnInit,OnDestroy {
-
-  private firstObsSubscription: Subscription
-  constructor() { }
+export class HomeComponent implements OnInit, OnDestroy {
+  private firstObsSubscription: Subscription;
+  constructor() {}
   ngOnDestroy(): void {
-    this.firstObsSubscription.unsubscribe()
+    this.firstObsSubscription.unsubscribe();
   }
 
   ngOnInit() {
     // this.firstObsSubscription = interval(1000).subscribe(count =>{
     //   console.log(count)
     // })
-    const customIntervalObservable = Observable.create(observer =>{
+    const customIntervalObservable = Observable.create((observer) => {
       let count = 0;
-      setInterval(() =>{observer.next(count),count++},1000)
+      setInterval(() => {
+        observer.next(count);
+        if (count === 2) {
+          observer.complete();
+        }
+        if (count > 3) {
+          observer.error(new Error("Count is greater 3!"));
+        }
+        count++;
+      }, 1000);
     });
-    this.firstObsSubscription = customIntervalObservable.subscribe(data=>{
-      console.log(data)
-    })
+    this.firstObsSubscription = customIntervalObservable.subscribe(
+      (data) => {
+        console.log(data);
+      },
+      (error) => {
+        console.log(error);
+        alert(error.message);
+      },
+      () => {
+        console.log("Completed!");
+      }
+    );
   }
-
 }
