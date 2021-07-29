a507d21a28b8dc84679c7862af9c70e22360f12f
Hamed Khademi Khaledi <khaledihkh@gmail.com>
2021-07-30 02:29:31 +0430

    TD Setting and Patching Form Values

--- 8/forms-td-start/src/app/app.component.ts
+++ 8/forms-td-start/src/app/app.component.ts
@@ -7,17 +7,23 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  @ViewChild('f', { static: false }) signUpForm:NgForm;
+  @ViewChild('f', { static: false }) signUpForm: NgForm;
   defaultQuestion = 'pet'
-  answer=''
+  answer = ''
   genders = ['male', 'female']
   suggestUserName() {
     const suggestedName = 'Superuser';
+    // this.signUpForm.setValue({
+    //   userData:{
+    //   username:suggestedName,
+    //   email:''
+    // },secret:'pet',questionAnswer:'',gender:'male'})
+    this.signUpForm.form.patchValue({ userData: { username: suggestedName } })
   }
   // onSubmit(form : NgForm){
   //   console.log(form)
   // }
-  onSubmit(){
-      console.log(this.signUpForm);
+  onSubmit() {
+    console.log(this.signUpForm);
   }
 }
