bb0efb536855f168bebe766ddb39b4d9106483d0
Hamed Khademi Khaledi <khaledihkh@gmail.com>
2021-07-30 02:37:56 +0430

    TD Using Form Data

--- 8/forms-td-start/src/app/app.component.ts
+++ 8/forms-td-start/src/app/app.component.ts
@@ -8,9 +8,18 @@ import { NgForm } from '@angular/forms';
 })
 export class AppComponent {
   @ViewChild('f', { static: false }) signUpForm: NgForm;
-  defaultQuestion = 'pet'
-  answer = ''
-  genders = ['male', 'female']
+  defaultQuestion = 'pet';
+  answer = '';
+  genders = ['male', 'female'];
+  user = {
+    username: '',
+    email: '',
+    secretQuestion: '',
+    answer: '',
+    gender: ''
+  };
+  submitted = false;
+
   suggestUserName() {
     const suggestedName = 'Superuser';
     // this.signUpForm.setValue({
@@ -24,6 +33,11 @@ export class AppComponent {
   //   console.log(form)
   // }
   onSubmit() {
-    console.log(this.signUpForm);
+    this.user.username = this.signUpForm.value.userData.username;
+    this.user.email = this.signUpForm.value.userData.email;
+    this.user.secretQuestion = this.signUpForm.value.secret;
+    this.user.answer = this.signUpForm.value.questionAnswer;
+    this.user.gender = this.signUpForm.value.gender;
+    this.submitted = true;
   }
 }
