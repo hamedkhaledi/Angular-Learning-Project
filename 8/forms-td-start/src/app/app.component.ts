545a4e95eb695774aed15ab32c7c87a0dcabc848
Hamed Khademi Khaledi <khaledihkh@gmail.com>
2021-07-30 02:04:05 +0430

    TD Set Default Values

--- 8/forms-td-start/src/app/app.component.ts
+++ 8/forms-td-start/src/app/app.component.ts
@@ -8,7 +8,8 @@ import { NgForm } from '@angular/forms';
 })
 export class AppComponent {
   @ViewChild('f', { static: false }) signUpForm:NgForm;
-
+  defaultQuestion = 'pet'
+  
   suggestUserName() {
     const suggestedName = 'Superuser';
   }
