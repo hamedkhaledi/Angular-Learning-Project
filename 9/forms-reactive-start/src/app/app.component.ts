e01bf86201c0b974541c4774ec730dbc4e9a4190
Hamed Khademi Khaledi <khaledihkh@gmail.com>
2021-07-30 19:26:30 +0430

    Reactive Grouping Controls

--- 9/forms-reactive-start/src/app/app.component.ts
+++ 9/forms-reactive-start/src/app/app.component.ts
@@ -12,8 +12,11 @@ export class AppComponent implements OnInit {
 
   ngOnInit(): void {
     this.signUpForm = new FormGroup({
-      username: new FormControl(null, Validators.required),
-      email: new FormControl(null, [Validators.required, Validators.email]),
+      userData: new FormGroup({
+        username: new FormControl(null, Validators.required),
+        email: new FormControl(null, [Validators.required, Validators.email]),
+      }),
+
       gender: new FormControl('male'),
     })
   }
