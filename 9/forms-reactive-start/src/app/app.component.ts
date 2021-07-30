04a2d8c22d0547c8d4e1c18330e38959414f1f34
Hamed Khademi Khaledi <khaledihkh@gmail.com>
2021-07-30 18:42:08 +0430

    Reactive Creating a Form in Code

--- 9/forms-reactive-start/src/app/app.component.ts
+++ 9/forms-reactive-start/src/app/app.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { Component, OnInit } from '@angular/core'
+import { FormControl, FormGroup } from '@angular/forms'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  genders = ['male', 'female'];
-  signUpForm: FormGroup;
+export class AppComponent implements OnInit {
+  genders = ['male', 'female']
+  signUpForm: FormGroup
+
+  ngOnInit(): void {
+    this.signUpForm = new FormGroup({
+      username: new FormControl(null),
+      email: new FormControl(null),
+      gender: new FormControl('male'),
+    })
+  }
 }
