15fd26353449b20d1ff2e44c6caf7875281b9b39
Hamed Khademi Khaledi <khaledihkh@gmail.com>
2021-07-31 22:15:52 +0430

    Loading the Shopping List Items into the Form

--- 2/shop-app/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ 2/shop-app/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnDestroy, OnInit } from '@angular/core'
+import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core'
 import { NgForm } from '@angular/forms'
 import { Subscription } from 'rxjs'
 import { Ingredient } from 'src/app/Shared/ingredient.model'
@@ -10,9 +10,11 @@ import { ShoppingListService } from '../shopping-list.service'
   styleUrls: ['./shopping-edit.component.css'],
 })
 export class ShoppingEditComponent implements OnInit, OnDestroy {
+  @ViewChild('f', { static: false }) slForm: NgForm
   subscription: Subscription
   editMode: boolean = false
   editedItemIndex: number
+  editedItem: Ingredient
   constructor(private slService: ShoppingListService) {}
 
   ngOnInit(): void {
@@ -20,6 +22,11 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
       (index: number) => {
         this.editMode = true
         this.editedItemIndex = index
+        this.editedItem = this.slService.getIngredient(index)
+        this.slForm.setValue({
+          name: this.editedItem.name,
+          amount: this.editedItem.amount,
+        })
       },
     )
   }
--- 2/shop-app/src/app/shopping-list/shopping-list.service.ts
+++ 2/shop-app/src/app/shopping-list/shopping-list.service.ts
@@ -1,30 +1,33 @@
-import { EventEmitter } from '@angular/core';
-import { Subject } from 'rxjs';
-import { Ingredient } from './../Shared/ingredient.model';
+import { EventEmitter } from '@angular/core'
+import { Subject } from 'rxjs'
+import { Ingredient } from './../Shared/ingredient.model'
 export class ShoppingListService {
-
   // ingredientsChanged = new EventEmitter<Ingredient[]>();
-  ingredientsChanged = new Subject<Ingredient[]>();
-  startedEditing = new Subject<number>();
+  ingredientsChanged = new Subject<Ingredient[]>()
+  startedEditing = new Subject<number>()
   ingredients: Ingredient[] = [
     new Ingredient('Apples', 5),
     new Ingredient('Tomatoes', 10),
-  ];
+  ]
 
   getIngredients() {
-    return this.ingredients.slice();
+    return this.ingredients.slice()
+  }
+
+  getIngredient(index: number) {
+    return this.ingredients[index]
   }
 
   addIngredient(ingredient: Ingredient) {
-    this.ingredients.push(ingredient);
-    this.ingredientsChanged.next(this.ingredients.slice());
+    this.ingredients.push(ingredient)
+    this.ingredientsChanged.next(this.ingredients.slice())
   }
 
   addIngredients(ingredients: Ingredient[]) {
     //   for (let ingredient of ingredients) {
     //       this.addIngredient(ingredient);
     //   }
-    this.ingredients.push(...ingredients);
-    this.ingredientsChanged.next(this.ingredients.slice());
+    this.ingredients.push(...ingredients)
+    this.ingredientsChanged.next(this.ingredients.slice())
   }
 }
