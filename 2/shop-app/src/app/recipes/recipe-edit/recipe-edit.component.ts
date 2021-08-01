a68de8ed2df5fde7ec4c23c87994e97541158263
Hamed Khademi Khaledi <khaledihkh@gmail.com>
2021-08-01 23:31:10 +0430

    Adding Ingredient Controls to a Form Array

--- 2/shop-app/src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ 2/shop-app/src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormArray, FormControl, FormGroup } from '@angular/forms';
 import { ActivatedRoute, Params } from '@angular/router';
 import { RecipeService } from '../recipe.service';
 
@@ -12,6 +12,7 @@ export class RecipeEditComponent implements OnInit {
   id: number;
   editMode = false;
   recipeForm: FormGroup;
+
   constructor(
     private route: ActivatedRoute,
     private recipeService: RecipeService
@@ -29,20 +30,38 @@ export class RecipeEditComponent implements OnInit {
     let recipeName = '';
     let recipeImagePath = '';
     let recipeDescription = '';
+    let recipeIngredients = new FormArray([]);
     if (this.editMode) {
       const recipe = this.recipeService.getRecipe(this.id);
       recipeName = recipe.name;
       recipeImagePath = recipe.imagePath;
       recipeDescription = recipe.description;
+      if (recipe['ingredients']) {
+        for (let ingredient of recipe.ingredients) {
+          recipeIngredients.push(
+            new FormGroup({
+              name: new FormControl(ingredient.name),
+              amount: new FormControl(ingredient.amount),
+            })
+          );
+        }
+      }
     }
     this.recipeForm = new FormGroup({
       name: new FormControl(recipeName),
       imagePath: new FormControl(recipeImagePath),
       description: new FormControl(recipeDescription),
+      ingredients: recipeIngredients,
     });
   }
 
   onSubmit() {
-    console.log(this.recipeForm)
+    console.log(this.recipeForm);
+  }
+
+  get controls() {
+    // a getter!
+    console.log(this.recipeForm.get('ingredients'));
+    return (<FormArray>this.recipeForm.get('ingredients')).controls;
   }
 }
