60e1b4b6304ace3f9e0644ee92a637e1949be6ee
Hamed Khademi Khaledi <khaledihkh@gmail.com>
2021-08-02 19:45:54 +0430

    Example Creating a Filter Pipe

--- 10/pipes-start/src/app/app.module.ts
+++ 10/pipes-start/src/app/app.module.ts
@@ -4,9 +4,10 @@ import { FormsModule } from "@angular/forms";
 
 import { AppComponent } from "./app.component";
 import { ShortenPipe } from "./shorten.pipe";
+import { FilterPipe } from './filter.pipe';
 
 @NgModule({
-  declarations: [AppComponent, ShortenPipe],
+  declarations: [AppComponent, ShortenPipe, FilterPipe],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
