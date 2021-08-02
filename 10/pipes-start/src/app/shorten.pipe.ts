a580e145a4758a417459aca78dd05bfcc91b1d93
Hamed Khademi Khaledi <khaledihkh@gmail.com>
2021-08-02 19:29:13 +0430

    Parametrizing a Custom Pipe

--- 10/pipes-start/src/app/shorten.pipe.ts
+++ 10/pipes-start/src/app/shorten.pipe.ts
@@ -4,9 +4,9 @@ import { Pipe, PipeTransform } from "@angular/core";
   name: "shorten",
 })
 export class ShortenPipe implements PipeTransform {
-  transform(value: any, ...args: any[]) {
-    if (value.length > 10) {
-      return value.substr(0, 10) + "...";
+  transform(value: any, limit: number, another: any) {
+    if (value.length > limit) {
+      return value.substr(0, limit) + "...";
     }
     return value;
   }
