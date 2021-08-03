75c886d78aa8627c27addc0bc0ab0cc964f4324c
Hamed Khademi Khaledi <khaledihkh@gmail.com>
2021-08-03 19:15:43 +0430

    GETting Data

--- 11/http-01-start/src/app/app.component.ts
+++ 11/http-01-start/src/app/app.component.ts
@@ -1,35 +1,49 @@
-import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core'
+import { HttpClient } from '@angular/common/http'
+import { templateJitUrl } from '@angular/compiler'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
-  loadedPosts = [];
+  loadedPosts = []
 
   constructor(private http: HttpClient) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.fetchPosts()
+  }
 
   onCreatePost(postData: { title: string; content: string }) {
     // Send Http request
     this.http
       .post(
         'https://ng-complete-guide-4f874-default-rtdb.firebaseio.com/posts.json',
-        postData
+        postData,
       )
-      .subscribe(responseData => {
-        console.log(responseData);
-      });
+      .subscribe((responseData) => {
+        console.log(responseData)
+      })
   }
 
   onFetchPosts() {
     // Send Http request
+    this.fetchPosts()
   }
 
   onClearPosts() {
     // Send Http request
+  }
+
+  private fetchPosts() {
+    this.http
+      .get(
+        'https://ng-complete-guide-4f874-default-rtdb.firebaseio.com/posts.json',
+      )
+      .subscribe((posts) => {
+        console.log(posts)
+      })
   }
 }
