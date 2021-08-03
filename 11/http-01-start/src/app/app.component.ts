3843c50c36126ad1d2c0c5ff08440fc199f08eb1
Hamed Khademi Khaledi <khaledihkh@gmail.com>
2021-08-03 19:42:45 +0430

    Using Types with the HttpClient

--- 11/http-01-start/src/app/app.component.ts
+++ 11/http-01-start/src/app/app.component.ts
@@ -1,36 +1,38 @@
-import { Component, OnInit } from '@angular/core'
-import { HttpClient } from '@angular/common/http'
-import { templateJitUrl } from '@angular/compiler'
-import { map } from 'rxjs/operators'
+import { Component, OnInit } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { templateJitUrl } from "@angular/compiler";
+import { map } from "rxjs/operators";
+import { Post } from "./post.model";
+const serverName = "https://ng-complete-guide-4f874-default-rtdb.firebaseio.com/posts.json";
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent implements OnInit {
-  loadedPosts = []
+  loadedPosts = [];
 
   constructor(private http: HttpClient) {}
 
   ngOnInit() {
-    this.fetchPosts()
+    this.fetchPosts();
   }
 
-  onCreatePost(postData: { title: string; content: string }) {
+  onCreatePost(postData: Post) {
     // Send Http request
     this.http
-      .post(
-        'https://ng-complete-guide-4f874-default-rtdb.firebaseio.com/posts.json',
-        postData,
+      .post<{ name: string }>(
+        serverName,
+        postData
       )
       .subscribe((responseData) => {
-        console.log(responseData)
-      })
+        console.log(responseData);
+      });
   }
 
   onFetchPosts() {
     // Send Http request
-    this.fetchPosts()
+    this.fetchPosts();
   }
 
   onClearPosts() {
@@ -39,21 +41,22 @@ export class AppComponent implements OnInit {
 
   private fetchPosts() {
     this.http
-      .get(
-        'https://ng-complete-guide-4f874-default-rtdb.firebaseio.com/posts.json',
+      .get<{ [key: string]: Post }>(
+        "https://ng-complete-guide-4f874-default-rtdb.firebaseio.com/posts.json"
       )
       .pipe(
         map((responseData) => {
-          const postsArray = []
+          const postsArray: Post[] = [];
           for (const key in responseData) {
             if (responseData.hasOwnProperty(key)) {
-              postsArray.push({ ...responseData[key], id: key })
+              postsArray.push({ ...responseData[key], id: key });
             }
           }
-        }),
+          return postsArray;
+        })
       )
       .subscribe((posts) => {
-        console.log(posts)
-      })
+        console.log(posts);
+      });
   }
 }
