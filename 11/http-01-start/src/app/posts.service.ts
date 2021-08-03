43e58f2c9e9f7cb3749e0e196731fcd427ae4712
Hamed Khademi Khaledi <khaledihkh@gmail.com>
2021-08-04 00:43:27 +0430

    Setting Headers

--- 11/http-01-start/src/app/posts.service.ts
+++ 11/http-01-start/src/app/posts.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http'
+import { HttpClient, HttpHeaders } from '@angular/common/http'
 import { Injectable } from '@angular/core'
 import { Subject, throwError } from 'rxjs'
 import { map, catchError } from 'rxjs/operators'
@@ -30,6 +30,9 @@ export class PostsService {
     return this.http
       .get<{ [key: string]: Post }>(
         'https://ng-complete-guide-4f874-default-rtdb.firebaseio.com/posts.json',
+        {
+          headers: new HttpHeaders({ 'Content-Type': 'application/json'}),
+        },
       )
       .pipe(
         map((responseData) => {
