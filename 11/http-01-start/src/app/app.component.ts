6c6a0af1c93d8b7bdf2e9d45f7493e9010a25926
Hamed Khademi Khaledi <khaledihkh@gmail.com>
2021-08-03 19:09:03 +0430

    Sending a POST Request

--- 11/http-01-start/src/app/app.component.ts
+++ 11/http-01-start/src/app/app.component.ts
@@ -17,7 +17,7 @@ export class AppComponent implements OnInit {
     // Send Http request
     this.http
       .post(
-        'https://ng-complete-guide-c56d3.firebaseio.com/posts.json',
+        'https://ng-complete-guide-4f874-default-rtdb.firebaseio.com/posts.json',
         postData
       )
       .subscribe(responseData => {
