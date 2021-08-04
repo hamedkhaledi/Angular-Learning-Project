6b2d96f59273a0fef7409aaee564f6974d264581
Hamed Khademi Khaledi <khaledihkh@gmail.com>
2021-08-04 20:05:09 +0430

    Storing Recipes

--- 2/shop-app/src/app/Shared/data-storage.service.ts
+++ 2/shop-app/src/app/Shared/data-storage.service.ts
@@ -1,7 +1,20 @@
 import { HttpClient } from '@angular/common/http'
 import { Injectable } from '@angular/core'
+import { Recipe } from '../recipes/recipe.model'
+import { RecipeService } from '../recipes/recipe.service'
 
 @Injectable({ providedIn: 'root' })
 export class DataStorageService {
-  constructor(http: HttpClient) {}
+  constructor(private http: HttpClient, private recipeService: RecipeService) {}
+  storeRecipes() {
+    const recipes = this.recipeService.getRecipes()
+    this.http
+      .put(
+        'https://ng-course-recipe-book-6d0c7-default-rtdb.firebaseio.com/recipes.json',
+        recipes,
+      )
+      .subscribe((response) => {
+        console.log(response)
+      })
+  }
 }
--- 2/shop-app/src/app/header/header.component.ts
+++ 2/shop-app/src/app/header/header.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import { DataStorageService } from '../Shared/data-storage.service'
 
 @Component({
   selector: 'app-header',
@@ -6,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
-  constructor() {}
+  constructor(private dataStorageService: DataStorageService) {}
 
   ngOnInit(): void {}
+  onSaveData() {
+    this.dataStorageService.storeRecipes()
+  }
 }
