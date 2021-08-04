870add1fa344fef8ee63aad4a5915d522c77a5bb
Hamed Khademi Khaledi <khaledihkh@gmail.com>
2021-08-04 18:52:00 +0430

    Setting Up the DataStorage Service

--- 2/shop-app/src/app/app.module.ts
+++ 2/shop-app/src/app/app.module.ts
@@ -1,21 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { RecipesComponent } from './recipes/recipes.component';
-import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
-import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
-import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
-import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
-import { DropdownDirective } from './Shared/dropdown.directive';
-import { ShoppingListService } from './shopping-list/shopping-list.service';
-import { AppRoutingModule } from './app-routing.module';
-import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
-import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
-import { RecipeService } from './recipes/recipe.service';
+import { BrowserModule } from '@angular/platform-browser'
+import { NgModule } from '@angular/core'
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'
+import { HttpClientModule } from '@angular/common/http'
+import { AppComponent } from './app.component'
+import { HeaderComponent } from './header/header.component'
+import { RecipesComponent } from './recipes/recipes.component'
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component'
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component'
+import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component'
+import { ShoppingListComponent } from './shopping-list/shopping-list.component'
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component'
+import { DropdownDirective } from './Shared/dropdown.directive'
+import { ShoppingListService } from './shopping-list/shopping-list.service'
+import { AppRoutingModule } from './app-routing.module'
+import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component'
+import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component'
+import { RecipeService } from './recipes/recipe.service'
 
 @NgModule({
   declarations: [
@@ -31,7 +31,13 @@ import { RecipeService } from './recipes/recipe.service';
     RecipeStartComponent,
     RecipeEditComponent,
   ],
-  imports: [BrowserModule, FormsModule, AppRoutingModule, ReactiveFormsModule],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    AppRoutingModule,
+    HttpClientModule,
+  ],
   providers: [ShoppingListService, RecipeService],
   bootstrap: [AppComponent],
 })
