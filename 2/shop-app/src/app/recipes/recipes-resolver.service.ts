09bdd1c8feeff3a1c67af4b6095a1a691dfb8f5f
Hamed Khademi Khaledi <khaledihkh@gmail.com>
2021-08-04 20:40:12 +0430

    Fixing a Bug with the Resolver

--- 2/shop-app/src/app/recipes/recipes-resolver.service.ts
+++ 2/shop-app/src/app/recipes/recipes-resolver.service.ts
@@ -7,14 +7,23 @@ import {
 import { Observable } from 'rxjs'
 import { DataStorageService } from '../Shared/data-storage.service'
 import { Recipe } from './recipe.model'
+import { RecipeService } from './recipe.service'
 
 @Injectable({ providedIn: 'root' })
 export class RecipeResolverService implements Resolve<Recipe[]> {
-  constructor(private dataStorageService: DataStorageService) {}
+  constructor(
+    private dataStorageService: DataStorageService,
+    private recipeService: RecipeService,
+  ) {}
   resolve(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot,
   ): Recipe[] | Observable<Recipe[]> | Promise<Recipe[]> {
-    return this.dataStorageService.fetchRecipes()
+    const recipes = this.recipeService.getRecipes()
+    if (recipes.length === 0) {
+      return this.dataStorageService.fetchRecipes()
+    } else {
+      return recipes
+    }
   }
 }
