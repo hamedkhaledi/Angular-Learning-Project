3b3a2344eec19bbb19de86458dd0e8ddf8194107
Hamed Khademi Khaledi <khaledihkh@gmail.com>
2021-08-05 22:17:57 +0430

    Sending the Signup Request

--- 2/shop-app/src/app/auth/auth/auth.component.ts
+++ 2/shop-app/src/app/auth/auth/auth.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core'
 import { NgForm } from '@angular/forms'
+import { AuthService } from './auth.service'
 
 @Component({
   selector: 'app-auth',
@@ -8,7 +9,7 @@ import { NgForm } from '@angular/forms'
 })
 export class AuthComponent implements OnInit {
   isLoginMode = true
-  constructor() {}
+  constructor(private authService: AuthService) {}
 
   ngOnInit(): void {}
 
@@ -17,7 +18,22 @@ export class AuthComponent implements OnInit {
   }
 
   onSubmit(form: NgForm) {
-    console.log(form)
+    if (!form.valid) {
+      return
+    }
+    const email = form.value.email
+    const password = form.value.password
+    if (this.isLoginMode) {
+    } else {
+      this.authService.signup(email, password).subscribe(
+        (resData) => {
+          console.log(resData)
+        },
+        (error) => {
+          console.log(error)
+        },
+      )
+    }
     form.reset()
   }
 }
