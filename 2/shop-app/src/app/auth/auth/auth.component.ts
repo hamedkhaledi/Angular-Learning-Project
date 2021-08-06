e8cbad5ed847faf45989d19f8165b742dfb1549e
Hamed Khademi Khaledi <khaledihkh@gmail.com>
2021-08-06 17:23:16 +0430

    Improving Error Handling

--- 2/shop-app/src/app/auth/auth/auth.component.ts
+++ 2/shop-app/src/app/auth/auth/auth.component.ts
@@ -34,9 +34,9 @@ export class AuthComponent implements OnInit {
           console.log(resData)
           this.isLoading = false
         },
-        (error) => {
-          console.log(error)
-          this.error = 'An error occurred'
+        (errorMessage) => {
+          console.log(errorMessage)
+          this.error = errorMessage
           this.isLoading = false
         },
       )
--- 2/shop-app/src/app/auth/auth/auth.service.ts
+++ 2/shop-app/src/app/auth/auth/auth.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http'
 import { Injectable } from '@angular/core'
+import { throwError } from 'rxjs'
+import { catchError } from 'rxjs/operators'
 
 interface AuthResponseData {
   kind: string
@@ -13,9 +15,8 @@ interface AuthResponseData {
 export class AuthService {
   constructor(private http: HttpClient) {}
   signup(email: string, password: string) {
-    return (
-      this.http.post <
-      AuthResponseData>(
+    return this.http
+      .post<AuthResponseData>(
         'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBvvRSYDySs6GUs6HMzzoiajhGL3PQoKOI',
         {
           email: email,
@@ -23,6 +24,18 @@ export class AuthService {
           returnSecureToken: true,
         },
       )
-    )
+      .pipe(
+        catchError((errorRes) => {
+          let errorMessage = 'An unknown error occurred'
+          if (!errorRes.error || !errorRes.error.error) {
+            return throwError(errorMessage)
+          }
+          switch (errorRes.error.error.message) {
+            case 'EMAIL_EXISTS':
+              errorMessage = 'this email exits already'
+          }
+          return throwError(errorMessage)
+        }),
+      )
   }
 }
