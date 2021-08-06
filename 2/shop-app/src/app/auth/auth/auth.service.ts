b566052fb7f98bc01e5cd0668fc5dcd9c5932dc7
Hamed Khademi Khaledi <khaledihkh@gmail.com>
2021-08-07 02:34:36 +0430

    Adding Auto-Login

--- 2/shop-app/src/app/auth/auth/auth.service.ts
+++ 2/shop-app/src/app/auth/auth/auth.service.ts
@@ -74,7 +74,9 @@ export class AuthService {
     const expirationDate = new Date(new Date().getTime() + expiresIn * 1000);
     const user = new User(email, userId, token, expirationDate);
     this.user.next(user);
+    localStorage.setItem('userData', JSON.stringify(user));
   }
+
   private handleError(errorRes: HttpErrorResponse) {
     let errorMessage = 'An unknown error occurred';
     if (!errorRes.error || !errorRes.error.error) {
@@ -91,6 +93,28 @@ export class AuthService {
         errorMessage = 'This password is not correct';
     }
     return throwError(errorMessage);
+  }
+
+  autoLogin() {
+    const userData: {
+      email: string;
+      id: string;
+      _token: string;
+      _tokenExpirationDate: string;
+    } = JSON.parse(localStorage.getItem('userData'));
+    if (!userData) {
+      return;
+    }
+    const loadedUser = new User(
+      userData.email,
+      userData.id,
+      userData._token,
+      new Date(userData._tokenExpirationDate)
+    );
+
+    if (loadedUser.token) {
+      this.user.next(loadedUser);
+    }
   }
 
   logout(): void {
