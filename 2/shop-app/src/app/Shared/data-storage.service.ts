424382bc4f2ddba66bb84adea3dcb019128ddf8e
Hamed Khademi Khaledi <khaledihkh@gmail.com>
2021-08-06 21:57:09 +0430

    Attaching the Token with an Interceptor

--- 2/shop-app/src/app/Shared/data-storage.service.ts
+++ 2/shop-app/src/app/Shared/data-storage.service.ts
@@ -23,25 +23,22 @@ export class DataStorageService {
       });
   }
   fetchRecipes() {
-    return this.authService.user.pipe(
-      take(1),
-      exhaustMap((user) => {
-        return this.http.get<Recipe[]>(
-          'https://ng-course-recipe-book-6d0c7-default-rtdb.firebaseio.com/recipes.json',
-          { params: new HttpParams().set('auth', user.token) }
-        );
-      }),
-      map((recipes) => {
-        return recipes.map((recipe) => {
-          return {
-            ...recipe,
-            ingredients: recipe.ingredients ? recipe.ingredients : [],
-          };
-        });
-      }),
-      tap((recipes) => {
-        return this.recipeService.setRecipes(recipes);
-      })
-    );
+    return this.http
+      .get<Recipe[]>(
+        'https://ng-course-recipe-book-6d0c7-default-rtdb.firebaseio.com/recipes.json'
+      )
+      .pipe(
+        map((recipes) => {
+          return recipes.map((recipe) => {
+            return {
+              ...recipe,
+              ingredients: recipe.ingredients ? recipe.ingredients : [],
+            };
+          });
+        }),
+        tap((recipes) => {
+          return this.recipeService.setRecipes(recipes);
+        })
+      );
   }
 }
---
+++ 2/shop-app/src/app/auth/auth-interceptor.service.ts
@@ -0,0 +1,33 @@
+import {
+  HttpEvent,
+  HttpHandler,
+  HttpInterceptor,
+  HttpParams,
+  HttpRequest,
+} from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { exhaustMap, take } from 'rxjs/operators';
+import { AuthService } from './auth/auth.service';
+
+@Injectable()
+export class AuthInterceptorService implements HttpInterceptor {
+  constructor(private authService: AuthService) {}
+  intercept(
+    req: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    return this.authService.user.pipe(
+      take(1),
+      exhaustMap((user) => {
+        if (!user) {
+          return next.handle(req);
+        }
+        const modifiedRequest = req.clone({
+          params: new HttpParams().set('auth', user.token),
+        });
+        return next.handle(req);
+      })
+    );
+  }
+}
