13739d80c6b2aa368a98b5d70b19b035e44723d0
Hamed Khademi Khaledi <khaledihkh@gmail.com>
2021-08-07 03:44:21 +0430

    Adding an Auth Guard

--- 2/shop-app/src/app/app-routing.module.ts
+++ 2/shop-app/src/app/app-routing.module.ts
@@ -1,12 +1,13 @@
-import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component'
-import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component'
-import { NgModule } from '@angular/core'
-import { RouterModule, Routes } from '@angular/router'
-import { RecipesComponent } from './recipes/recipes.component'
-import { ShoppingListComponent } from './shopping-list/shopping-list.component'
-import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component'
-import { RecipeResolverService } from './recipes/recipes-resolver.service'
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { RecipesComponent } from './recipes/recipes.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
+import { RecipeResolverService } from './recipes/recipes-resolver.service';
 import { AuthComponent } from './auth/auth/auth.component';
+import { AuthGuard } from './auth/auth/auth.guard';
 
 const appRoutes: Routes = [
   {
@@ -17,6 +18,7 @@ const appRoutes: Routes = [
   {
     path: 'recipes',
     component: RecipesComponent,
+    canActivate: [AuthGuard],
     children: [
       {
         path: '',
@@ -46,7 +48,7 @@ const appRoutes: Routes = [
     path: 'auth',
     component: AuthComponent,
   },
-]
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(appRoutes)],
