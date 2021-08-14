d7e2c08290e5605f7dd064cc68ffdd99383bd859
Hamed Khademi Khaledi <khaledihkh@gmail.com>
2021-08-14 23:30:26 +0430

    change consts actions

--- 12/ngrx-12-finished/src/app/auth/store/auth.actions.ts
+++ 12/ngrx-12-finished/src/app/auth/store/auth.actions.ts
@@ -1,7 +1,7 @@
 import { Action } from "@ngrx/store";
 
-export const LOGIN = "LOGIN";
-export const LOGOUT = "LOGOUT";
+export const LOGIN = "[Auth] Login";
+export const LOGOUT = "[Auth] Logout";
 
 export class Login implements Action {
   readonly type = LOGIN;
