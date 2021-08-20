750306dd0eb9f452ab6bb9a63d1dfcbc9e41169d
Hamed Khademi Khaledi <khaledihkh@gmail.com>
2021-08-20 06:58:44 +0430

    Animations Triggers and State

--- 13/animations/src/app/app.component.ts
+++ 13/animations/src/app/app.component.ts
@@ -1,13 +1,27 @@
-import { Component } from '@angular/core';
+import { state, style, trigger } from "@angular/animations";
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html'
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  animations: [
+    trigger("divState", [
+      state(
+        "normal",
+        style({ backgroundColor: "red", transform: "translateX(0)" })
+      ),
+      state(
+        "highlighted",
+        style({ backgroundColor: "blue", transform: "translateX(100px)" })
+      ),
+    ]),
+  ],
 })
 export class AppComponent {
-  list = ['Milk', 'Sugar', 'Bread'];
+  state = "normal";
+  list = ["Milk", "Sugar", "Bread"];
 
-    onAdd(item) {
-      this.list.push(item);
-    }
+  onAdd(item) {
+    this.list.push(item);
+  }
 }
