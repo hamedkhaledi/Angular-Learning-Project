fe83be7de5da01047e9e68379c5d573cee3d64d3
Hamed Khademi Khaledi <khaledihkh@gmail.com>
2021-08-20 07:17:47 +0430

    Advanced Transitions

--- 13/animations/src/app/app.component.ts
+++ 13/animations/src/app/app.component.ts
@@ -20,22 +20,52 @@ import { Component } from "@angular/core";
         "highlighted",
         style({ backgroundColor: "blue", transform: "translateX(100px)" })
       ),
+      transition("normal <=> highlighted", animate(400)),
+      // transition("highlighted => normal", animate(800)),
+    ]),
+    trigger("wildState", [
+      state(
+        "normal",
+        style({ backgroundColor: "red", transform: "translateX(0) scale(1)" })
+      ),
+      state(
+        "highlighted",
+        style({
+          backgroundColor: "blue",
+          transform: "translateX(100px) scale(1)",
+        })
+      ),
+      state(
+        "shrunken",
+        style({
+          backgroundColor: "green",
+          transform: "translateX(0) scale(0.5)",
+        })
+      ),
       transition("normal => highlighted", animate(300)),
       transition("highlighted => normal", animate(800)),
+      transition("shrunken <=> *", animate(500)),
     ]),
   ],
 })
 export class AppComponent {
   state = "normal";
+  wildState = "normal";
   list = ["Milk", "Sugar", "Bread"];
 
   onAnimate() {
     this.state == "normal"
       ? (this.state = "highlighted")
       : (this.state = "normal");
+    this.wildState == "normal"
+      ? (this.wildState = "highlighted")
+      : (this.wildState = "normal");
   }
   onAdd(item) {
     this.list.push(item);
+  }
+  onShrink() {
+    this.wildState = "shrunken";
   }
   onDelete(item) {
     this.list.splice(this.list.indexOf(item), 1);
