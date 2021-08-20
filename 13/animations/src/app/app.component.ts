92697a84b42c21d6bf678b0ad5cdeee5babd1026
Hamed Khademi Khaledi <khaledihkh@gmail.com>
2021-08-20 07:48:06 +0430

    Using Keyframes for Animations

--- 13/animations/src/app/app.component.ts
+++ 13/animations/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import {
   animate,
+  keyframes,
   state,
   style,
   transition,
@@ -26,28 +27,38 @@ import { Component } from "@angular/core";
     trigger("wildState", [
       state(
         "normal",
-        style({ backgroundColor: "red", transform: "translateX(0) scale(1)" })
+        style({
+          "background-color": "red",
+          transform: "translateX(0) scale(1)",
+        })
       ),
       state(
         "highlighted",
         style({
-          backgroundColor: "blue",
+          "background-color": "blue",
           transform: "translateX(100px) scale(1)",
         })
       ),
       state(
         "shrunken",
         style({
-          backgroundColor: "green",
+          "background-color": "green",
           transform: "translateX(0) scale(0.5)",
         })
       ),
       transition("normal => highlighted", animate(300)),
       transition("highlighted => normal", animate(800)),
       transition("shrunken <=> *", [
-        style({ backgroundColor: "orange" }),
-        animate(1000, style({ borderRadius: "50px" })),
-        animate(5000),
+        style({
+          "background-color": "orange",
+        }),
+        animate(
+          1000,
+          style({
+            borderRadius: "50px",
+          })
+        ),
+        animate(500),
       ]),
     ]),
     trigger("list1", [
@@ -55,6 +66,27 @@ import { Component } from "@angular/core";
       transition("void => *", [
         style({ opacity: "0", transform: "translateX(-100px)" }),
         animate(300),
+      ]),
+      transition("* => void", [
+        animate(300, style({ opacity: "0", transform: "translateX(100px)" })),
+      ]),
+    ]),
+    trigger("list2", [
+      state("in", style({ opacity: "1", transform: "translateX(0)" })),
+      transition("void => *", [
+        animate(
+          1000,
+          keyframes([
+            style({ transform: "translateX(-100px)", opacity: 0, offset: 0 }),
+            style({
+              transform: "translateX(-50px)",
+              opacity: 0.5,
+              offset: 0.7,
+            }),
+            style({ transform: "translateX(-20px)", opacity: 1, offset: 0.8 }),
+            style({ transform: "translateX(0px)", opacity: 1, offset: 1 }),
+          ])
+        ),
       ]),
       transition("* => void", [
         animate(300, style({ opacity: "0", transform: "translateX(100px)" })),
